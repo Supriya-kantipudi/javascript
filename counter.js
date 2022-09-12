0c0e2efc56ae9987792326a306b58c6c92c8da3f
Supriya-kantipudi <kantipudisupriya2003@gmail.com>
2022-09-12 21:09:35 +0530

    completed add and sub

--- counter.js
+++ counter.js
@@ -3,3 +3,16 @@ let remove=document.getElementById('decreament');
 
 let int = document.getElementById('number');
 let integer = 0;
+
+add.addEventListener('click',function(){
+    integer += 1;
+    int.innerHTML=integer;
+})
+
+remove.addEventListener('click',function(){
+    integer -= 1;
+    if(integer < 0){
+        integer = 0;
+    }
+    int.innerHTML = integer;
+})
